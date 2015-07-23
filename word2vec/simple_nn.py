cc53deea6c6f5db2d7922e00227fcb7a912dee57
Peter Henderson <peter.henderson@mail.mcgill.ca>
2015-07-22 20:56:06 -0400

    meh

--- word2vec/simple_nn.py
+++ word2vec/simple_nn.py
@@ -3,6 +3,11 @@ from softmax import softmax
 
 import numpy as np
 
+###################################################################
+# Note, I used msushkov's implementation to double check my own.
+# Mine was not great, so you'll see a lot of similarities to his.
+# https://github.com/msushkov/cs224d-hw1/blob/master/wordvec_sentiment.ipynb
+###################################################################
 
 def forward_backward_prop(data, labels, params):
     """ Forward and backward propagation for a two-layer sigmoidal network """
@@ -10,7 +15,7 @@ def forward_backward_prop(data, labels, params):
     # Compute the forward propagation and for the cross entropy cost, #
     # and backward propagation for the gradients for all parameters.  #
     ###################################################################
-    
+
     ### Unpack network parameters (do not modify)
     t = 0
     W1 = np.reshape(params[t:t+dimensions[0]*dimensions[1]], (dimensions[0], dimensions[1]))
@@ -20,29 +25,37 @@ def forward_backward_prop(data, labels, params):
     W2 = np.reshape(params[t:t+dimensions[1]*dimensions[2]], (dimensions[1], dimensions[2]))
     t += dimensions[1]*dimensions[2]
     b2 = np.reshape(params[t:t+dimensions[2]], (1, dimensions[2]))
-    
+
     ### YOUR CODE HERE: forward propagation
-    
+
     # cost = ...
-    Z1 = np.dot(W1, data) + b1
-    A1 = sigmoid(Z1)
-    Z2 = np.dot(W1, A1) + b2
-    A2 = softmax(Z2)
+    h = sigmoid(np.dot(W1, data) + b1) # hidden layer
+    y_hat = softmax(np.dot(W2, h) + b2) # Top classifier layer
     N, D = data.shape
-    cost = (-1/N)*np.sum(np.log(A2))
+
+    # TODO: may need to change this to sum over rows and then sum up rows?
+    cost = (-1/N)*np.sum(np.dot(labels, np.log(A2)))
 
     ### END YOUR CODE
-    
+
     ### YOUR CODE HERE: backward propagation
 
     #gradW1 = ...
     #gradb1 = ...
     #gradW2 = ...
     #gradb2 = ...
-    
+
+    # d_y_hat/d_W2
+    gradW2 = np.dot((y_hat - labels), h)
+    gradW1 = np.do
+    assert gradW1.shape == W1.shape
+    assert gradb1.shape == b1.shape
+    assert W2.shape == gradW2.shape
+    assert gradb2.shape == b2.shape
+
     ### END YOUR CODE
-    
+
     ### Stack gradients (do not modify)
     grad = np.concatenate((gradW1.flatten(), gradb1.flatten(), gradW2.flatten(), gradb2.flatten()))
-    
+
     return cost, grad
