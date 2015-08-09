605a1af9448cc67c83b157e8a39e338da4667970
Peter Henderson <peter.henderson@mail.mcgill.ca>
2015-08-09 15:29:11 -0400

    More progress, now paralleling A2

--- xor/neural_xor.py
+++ xor/neural_xor.py
@@ -2,14 +2,19 @@ import argparse
 from nn.math import sigmoid
 from numpy import *
 from matplotlib.pyplot import *
+
+# The goal of this is to demonstrate how to set weights of a two layer neural net to
+# simulate an XOR function
+
+# matplotlib settings
 matplotlib.rcParams['savefig.dpi'] = 100
 
+# arg parser whether to show the graphs or just save them
 parser = argparse.ArgumentParser(description='Run through and manually set weights for a network.')
 parser.add_argument('--data_graphs', help='Show initial demonstrative data graphs.')
 args = parser.parse_args()
 
-# TODO: Add instructions
-
+# data display functionality
 colors = 'rbcm'
 markers = 'soos'
 def show_pts(data):
@@ -32,6 +37,8 @@ npts = 4 * 40; random.seed(10)
 x = random.randn(npts)*0.1 + array([i & 1 for i in range(npts)])
 y = random.randn(npts)*0.1 + array([(i & 2) >> 1 for i in range(npts)])
 data = vstack([x,y])
+
+# if we want to show the initial data graphs then show them
 if args.data_graphs:
     fig = figure(figsize=(4,4)); show_pts(data); ylim(-0.5, 1.5); xlim(-0.5, 1.5)
     xlabel("x"); ylabel("y"); title("Input Data")
@@ -51,14 +58,14 @@ b1 = zeros((2,1))
 U = zeros(2)
 b2 = 0
 
-# Your task is to add wieght functions to manually separate the data
-#### YOUR CODE HERE ####
+# Your task is to add weight functions to manually separate the data
+#### REPLACE THESE IF YOU WANT TO MESS AROUND WITH WEIGHTS ####
 W = array([[-1.0,-1.0],[1.0,1.0]])
 b1 = array([[.5],[-1.5]])
 U = array([1.0, 1.0])
 b2 = -.5
 z = 5
-#### END YOUR CODE ####
+#### END REPLACING STUFF ####
 
 # Feed-forward computation
 h = sigmoid(z*(W.dot(data) + b1))
@@ -68,6 +75,8 @@ p = sigmoid(z*(U.dot(h) + b2))
 subplot(1,2,1); show_pts(h)
 title("Hidden Layer"); xlabel("$h_1$"); ylabel("$h_2$")
 ylim(-0.1, 1.1); xlim(-0.1, 1.1)
+
+# Save to temp file
 tmpfile = '/tmp/hidden_layer.png'
 savefig(tmpfile)
 print "Hidden layer image saved to {}".format(tmpfile)
@@ -77,6 +86,7 @@ subplot(1,2,2); show_pts_1d(p)
 title("Output"); ylabel("Prediction"); xticks([])
 axhline(0.5, linestyle='--', color='k')
 
+# Save to temp file
 tmpfile = '/tmp/output_layer.png'
 savefig(tmpfile)
 print "Output layer image saved to {}".format(tmpfile)
